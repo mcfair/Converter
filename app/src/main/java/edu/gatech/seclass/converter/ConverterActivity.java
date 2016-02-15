6c7cb34b8ad78d608f2590f32e7cd9c9ba5c303b
Huiyang Fei <Huiyang.Fei@gatech.edu>
2016-02-14 21:44:16 -0700

    Initial commit

---
+++ app/src/main/java/edu/gatech/seclass/converter/ConverterActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.seclass.converter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
